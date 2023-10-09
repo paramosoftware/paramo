0e307f22a3f69833666ec47e31c00c0704c8e7e0
Matheus <dmatheusps@gmail.com>
2023-10-09 17:01:11 -0300

    IG-137 Escapar aspas duplas dentro de html values

--- app/components/campo_autocomplete.php
+++ app/components/campo_autocomplete.php
@@ -397,7 +397,7 @@
                    maxlength="<?php print $vn_tamanho_maximo; ?>"
                    name="<?php print $vs_nome_campo_lookup ?>"
                    id="<?php print $vs_nome_campo_lookup ?>"
-                   value="<?php print $vs_valor_campo_nome;
+                   value="<?php print htmlentities($vs_valor_campo_nome, ENT_QUOTES, "UTF-8", false);
                    ?>"
     <?php
         }
