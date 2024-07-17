2d7b245e1b8f2f1e5c9414ec7db58006d11adbec
smllb <samuelbarbosa.ipms@gmail.com>
2024-07-17 00:20:32 -0300

    feat: add step 3 and 4 functions

--- src/lib/system/importacao_refatorado.php
+++ src/lib/system/importacao_refatorado.php
@@ -9,7 +9,7 @@ class importacao_refatorado
 
     private $duracao;
 
-    private $operacoes;
+    private $operacoes = array();
 
     // Parametros importacao
 
@@ -27,15 +27,16 @@ class importacao_refatorado
 
     private $objeto_importacao;
 
-    private $campos_edicao;
+    private $campos_edicao = array();
 
-    private $campos_relacionamento;
+    private $campos_relacionamento = array();
 
     // Selecoes do usuário & Origem de dados
-    private $header_campos_origem;
-    private $selecao_campos_destino;
+    private $campos_origem_label = array();
+    private $campos_destino_selecao = array();
 
-    private $dados_origem;
+    private $dados_origem = array();
+    private $campos_variantes_importacao = array();
 
     function __construct($ps_id_objeto_importacao = null)
     {
@@ -44,6 +45,45 @@ class importacao_refatorado
             $this->inicializar_objeto_importacao();
         }
 
+    }
+    public function inicializar_timezone_importacao() {
+        $this->timezone = new DateTimeZone('America/Sao_Paulo');
+    }
+    public function inicializar_data_inicio_importacao() {
+        $this->inicio = new DateTime('now', $this->timezone);
+    }
+    public function get_usuario_relacionou_identificador($pb_is_item_acervo): int|null {
+        foreach(array_keys($this->campos_destino_selecao) as $vn_posicao_campo_destino => $vs_identificacao_campo_destino) {
+            if (str_contains($vs_identificacao_campo_destino, $pb_is_item_acervo ? "_identificador" : "_nome")) {
+                return $vn_posicao_campo_destino;
+            }
+        }
+        return null;
+    }
+    public function importar(): array {
+        foreach ($this->dados_origem as $va_linha_importacao) {
+            $this->processar_linha($va_linha_importacao);
+        }
+        return $this->operacoes;
+    }
+    public function processar_linha($pa_linha_importacao) {
+        $va_dados_insercao_linha = array();
+
+        if (in_array($this->modo_importacao, ["upsert", "update", "create"])) {
+            if ($this->is_item_acervo($this->objeto_importacao)) {
+
+            }
+
+            $vn_index_identificador_objeto_importacao = $this->get_usuario_relacionou_identificador($this->is_item_acervo($this->objeto_importacao));
+            // ...
+        }
+
+    }
+    public function processar_coluna() {
+
+    }
+    public function is_item_acervo($pa_id_objeto) {
+        return is_subclass_of(new $pa_id_objeto, "texto");
     }
     public function inicializar_objeto_importacao() {
         $this->objeto_importacao = new $this->id_objeto_importacao;
@@ -54,7 +94,18 @@ class importacao_refatorado
         }
 
     }
+    public function inicializar_variantes_campos_importacao($pa_valores_padrao, $pa_criar_itens_relacionados, $pa_separadores_valores, $pa_separadores_subcampo,): void {
+        foreach (array_keys($this->campos_destino_selecao) as $vn_posicao_campo_destino) {
+            $this->campos_variantes_importacao[$vn_posicao_campo_destino]  = [
+                "valor_padrao" => $pa_valores_padrao[$vn_posicao_campo_destino],
+                "separador_subcampos" => $pa_separadores_subcampo[$vn_posicao_campo_destino],
+                "separador_valores" => $pa_separadores_valores[$vn_posicao_campo_destino],
+                "criar_itens_relacionados" => isset($pa_criar_itens_relacionados[$vn_posicao_campo_destino])
+                // [tipo de relacao padrao]
 
+            ];
+        }
+    }
     public function get_operacoes(): array
     {
         return $this->operacoes;
@@ -94,22 +145,31 @@ class importacao_refatorado
     {
         return $this->campos_edicao;
     }
+    public function get_campo_edicao($ps_label): array
+    {
+        return array_key_exists($ps_label, $this->campos_edicao) ? $this->campos_edicao[$ps_label] : [];
 
+    }
     public function get_campos_relacionamento(): array
     {
         return $this->campos_relacionamento;
     }
 
-    public function get_header_campos_origem(): array
+    public function get_campos_origem_label(): array
     {
-        return $this->header_campos_origem;
+        return $this->campos_origem_label;
     }
-
-    public function get_selecao_campos_destino(): array
+    public function get_campo_origem_label($pn_index_selecao):  string {
+        return $pn_index_selecao <= count($this->campos_origem_label) ? $this->campos_origem_label[$pn_index_selecao] : "";
+    }
+    public function get_campos_destino_selecao(): array
     {
-        return $this->selecao_campos_destino;
+        return $this->campos_destino_selecao;
     }
+    public function get_campo_destino_selecao($pn_index_selecao): string {
+        return $pn_index_selecao <= count($this->campos_destino_selecao) ? $this->campos_destino_selecao[$pn_index_selecao] : "";
 
+    }
     public function get_dados_origem(): array
     {
         return $this->dados_origem;
@@ -123,14 +183,16 @@ class importacao_refatorado
         $this->dados_origem = $dados_origem;
     }
 
-    public function set_selecao_campos_destino($selecao_campos_destino): void
+    public function set_campos_destino_selecao($pa_campos_destino_selecao): void
     {
-        $this->selecao_campos_destino = $selecao_campos_destino;
+        $this->campos_destino_selecao = array_filter($pa_campos_destino_selecao, function($vs_campo) {
+            return !empty($vs_campo);
+        });
     }
 
-    public function set_header_campos_origem($header_campos_origem): void
+    public function set_campos_origem_label($campos_origem_label): void
     {
-        $this->header_campos_origem = $header_campos_origem;
+        $this->campos_origem_label = $campos_origem_label;
     }
 
     public function set_campos_relacionamento($campos_relacionamento): void
