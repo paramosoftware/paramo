a460083526aa2835cb65b6ce4a5923575d6ff68f
Frederico Camargo <fredcamargo@yahoo.com>
2025-07-25 15:14:03 -0300

    FDP-534 Modificar a posição do botão de "fechar imagem" para ser mais próximo a imagem

--- app/editar.php
+++ app/editar.php
@@ -453,10 +453,6 @@ require_once dirname(__FILE__) . "/components/entry_point.php";
                                         <?php
                                         }
                                         ?>
-
-                                        <div class="btn-group me-2" role="group" aria-label="First group">
-                                            <button class="btn btn-outline-primary btn_fechar_imagem" type="button" id="btn_fechar_imagem" style="display:none">Fechar imagem</button>
-                                        </div>
                                     </div>
                                     
                                     <?php
